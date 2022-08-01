1e299c59b43c42ce020a6be120db2e6085e798e6
cgriffin21 <chance@thechanceproject.org>
2022-08-01 07:00:48 -0700

    remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,6 @@ import { Provider } from 'react-redux';
 import App from 'next/app';
 import Head from 'next/head';
 import withRedux from 'next-redux-wrapper';
-import { initStore } from '../store/reducers/cartReducer';
 import Loader from '../components/Shared/Loader';
 import GoTop from '../components/Shared/GoTop';
 
